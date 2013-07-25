0af54055345f44e32aed4d59e17d7832d47dafca
oaky <federicorojkin@gmail.com>
2013-07-25 16:11:55 -0300

    dd

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Athenas::Application.config.secret_key_base = '6eef651e3d1808144a6224dab6a9174d60888a9e627d9e1170c9af3b850bb2c2a252494944168d59a40a522676b42bf4e6aa101c2b633fcce6f54db693ba294b'
